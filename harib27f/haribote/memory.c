f35e0ea7b1ec2964c84b73fbbe550e14aa29a8eb
HopeKing1997 <834711075@qq.com>
2018-01-03 10:32:49 +0800

    Revert "第一部分"
    This reverts commit 90c2ac7547b3c066adc14ffc351d9887322f5bfb.

--- harib27f/haribote/memory.c
+++ harib27f/haribote/memory.c
@@ -10,12 +10,12 @@ unsigned int memtest(unsigned int start, unsigned int end)
 	char flg486 = 0;
 	unsigned int eflg, cr0, i;
 
-	/* 确认CPU是386还是486*/
+	/* 386偐丄486埲崀側偺偐偺妋擣 */
 	eflg = io_load_eflags();
 	eflg |= EFLAGS_AC_BIT; /* AC-bit = 1 */
 	io_store_eflags(eflg);
 	eflg = io_load_eflags();
-	if ((eflg & EFLAGS_AC_BIT) != 0) { /* 若果是386即使设定AC=1，AC还是会自动归零 */
+	if ((eflg & EFLAGS_AC_BIT) != 0) { /* 386偱偼AC=1偵偟偰傕帺摦偱0偵栠偭偰偟傑偆 */
 		flg486 = 1;
 	}
 	eflg &= ~EFLAGS_AC_BIT; /* AC-bit = 0 */
@@ -23,7 +23,7 @@ unsigned int memtest(unsigned int start, unsigned int end)
 
 	if (flg486 != 0) {
 		cr0 = load_cr0();
-		cr0 |= CR0_CACHE_DISABLE; /*禁止缓存 */
+		cr0 |= CR0_CACHE_DISABLE; /* 僉儍僢僔儏嬛巭 */
 		store_cr0(cr0);
 	}
 
@@ -31,7 +31,7 @@ unsigned int memtest(unsigned int start, unsigned int end)
 
 	if (flg486 != 0) {
 		cr0 = load_cr0();
-		cr0 &= ~CR0_CACHE_DISABLE; /* 允许缓存 */
+		cr0 &= ~CR0_CACHE_DISABLE; /* 僉儍僢僔儏嫋壜 */
 		store_cr0(cr0);
 	}
 
@@ -40,15 +40,15 @@ unsigned int memtest(unsigned int start, unsigned int end)
 
 void memman_init(struct MEMMAN *man)
 {
-	man->frees = 0;			/* 可用信息数目 */
-	man->maxfrees = 0;		/* 用于观察可用状况 */
-	man->lostsize = 0;		/* 释放失败的内存大小综合 */
-	man->losts = 0;			/* 释放失败的次数 */
+	man->frees = 0;			/* 偁偒忣曬偺屄悢 */
+	man->maxfrees = 0;		/* 忬嫷娤嶡梡丗frees偺嵟戝抣 */
+	man->lostsize = 0;		/* 夝曻偵幐攕偟偨崌寁僒僀僘 */
+	man->losts = 0;			/* 夝曻偵幐攕偟偨夞悢 */
 	return;
 }
 
 unsigned int memman_total(struct MEMMAN *man)
-/* 报告空余内存大小总和 */
+/* 偁偒僒僀僘偺崌寁傪曬崘 */
 {
 	unsigned int i, t = 0;
 	for (i = 0; i < man->frees; i++) {
@@ -58,20 +58,20 @@ unsigned int memman_total(struct MEMMAN *man)
 }
 
 unsigned int memman_alloc(struct MEMMAN *man, unsigned int size)
-/* 分配 */
+/* 妋曐 */
 {
 	unsigned int i, a;
 	for (i = 0; i < man->frees; i++) {
 		if (man->free[i].size >= size) {
-			/* 找到足够大的内存 */
+			/* 廫暘側峀偝偺偁偒傪敪尒 */
 			a = man->free[i].addr;
 			man->free[i].addr += size;
 			man->free[i].size -= size;
 			if (man->free[i].size == 0) {
-				/* free[i].size变成0就剪掉一条可用信息 */
+				/* free[i]偑側偔側偭偨偺偱慜傊偮傔傞 */
 				man->frees--;
 				for (; i < man->frees; i++) {
-					man->free[i] = man->free[i + 1]; /* 带入结构体 */
+					man->free[i] = man->free[i + 1]; /* 峔憿懱偺戙擖 */
 				}
 			}
 			return a;
@@ -81,11 +81,11 @@ unsigned int memman_alloc(struct MEMMAN *man, unsigned int size)
 }
 
 int memman_free(struct MEMMAN *man, unsigned int addr, unsigned int size)
-/* 释放内存 */
+/* 夝曻 */
 {
 	int i, j;
-	/* 为便于归纳内存，将free[i]按照addr顺序排列 */
-	/* 所以，先决定应该放在哪里 */
+	/* 傑偲傔傗偡偝傪峫偊傞偲丄free[]偑addr弴偵暲傫偱偄傞傎偆偑偄偄 */
+	/* 偩偐傜傑偢丄偳偙偵擖傟傞傋偒偐傪寛傔傞 */
 	for (i = 0; i < man->frees; i++) {
 		if (man->free[i].addr > addr) {
 			break;
@@ -93,58 +93,58 @@ int memman_free(struct MEMMAN *man, unsigned int addr, unsigned int size)
 	}
 	/* free[i - 1].addr < addr < free[i].addr */
 	if (i > 0) {
-		/* 前面有可用内存 */
+		/* 慜偑偁傞 */
 		if (man->free[i - 1].addr + man->free[i - 1].size == addr) {
-			/* 与前面的内存归纳到一起 */
+			/* 慜偺偁偒椞堟偵傑偲傔傜傟傞 */
 			man->free[i - 1].size += size;
 			if (i < man->frees) {
-				/* 后面也有 */
+				/* 屻傠傕偁傞 */
 				if (addr + size == man->free[i].addr) {
-					/* 与后面的可用内存归纳到一起 */
+					/* 側傫偲屻傠偲傕傑偲傔傜傟傞 */
 					man->free[i - 1].size += man->free[i].size;
-					/* man->free[i]删除 */
-					/* free[i]变成0后归纳到前面去 */
+					/* man->free[i]偺嶍彍 */
+					/* free[i]偑側偔側偭偨偺偱慜傊偮傔傞 */
 					man->frees--;
 					for (; i < man->frees; i++) {
-						man->free[i] = man->free[i + 1]; /* 结构体赋值 */
+						man->free[i] = man->free[i + 1]; /* 峔憿懱偺戙擖 */
 					}
 				}
 			}
-			return 0; /* 成功完成 */
+			return 0; /* 惉岟廔椆 */
 		}
 	}
-	/* 不能与前面的可用空间归纳到一起 */
+	/* 慜偲偼傑偲傔傜傟側偐偭偨 */
 	if (i < man->frees) {
-		/* 后面还有 */
+		/* 屻傠偑偁傞 */
 		if (addr + size == man->free[i].addr) {
-			/* 和后面的空间归纳到一起 */
+			/* 屻傠偲偼傑偲傔傜傟傞 */
 			man->free[i].addr = addr;
 			man->free[i].size += size;
-			return 0; /* 成功完成 */
+			return 0; /* 惉岟廔椆 */
 		}
 	}
-	/* 既不能与前面也不能与后面合并到一起 */
+	/* 慜偵傕屻傠偵傕傑偲傔傜傟側偄 */
 	if (man->frees < MEMMAN_FREES) {
-		/* free[i]之后的空间向后移动，腾出一个可用空间 */
+		/* free[i]傛傝屻傠傪丄屻傠傊偢傜偟偰丄偡偒傑傪嶌傞 */
 		for (j = man->frees; j > i; j--) {
 			man->free[j] = man->free[j - 1];
 		}
 		man->frees++;
 		if (man->maxfrees < man->frees) {
-			man->maxfrees = man->frees; /* 更新最大值 */
+			man->maxfrees = man->frees; /* 嵟戝抣傪峏怴 */
 		}
 		man->free[i].addr = addr;
 		man->free[i].size = size;
-		return 0; /* 成功完成 */
+		return 0; /* 惉岟廔椆 */
 	}
-	/* 空闲空间移出导致不能往后移动 */
+	/* 屻傠偵偢傜偣側偐偭偨 */
 	man->losts++;
 	man->lostsize += size;
-	return -1; /* 失败 */
+	return -1; /* 幐攕廔椆 */
 }
 
 unsigned int memman_alloc_4k(struct MEMMAN *man, unsigned int size)
-{//以0x1000（4k）字节为单位进行内存分配 
+{
 	unsigned int a;
 	size = (size + 0xfff) & 0xfffff000;
 	a = memman_alloc(man, size);
@@ -152,7 +152,7 @@ unsigned int memman_alloc_4k(struct MEMMAN *man, unsigned int size)
 }
 
 int memman_free_4k(struct MEMMAN *man, unsigned int addr, unsigned int size)
-{//以0x1000（4k）字节为单位进行内存释放
+{
 	int i;
 	size = (size + 0xfff) & 0xfffff000;
 	i = memman_free(man, addr, size);
